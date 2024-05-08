0ee8f8425b56dfde4a30981e592e02255f719489
Charan Raj Kandula <112989454+rajkandula@users.noreply.github.com>
2024-05-08 08:33:13 -0500

    Create plus.swift

---
+++ plus.swift
@@ -0,0 +1,50 @@
+  var body: some View {
+        VStack {
+            Image(systemName: "plus")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            HStack{
+                Text("Hello, world!")
+                Text("Hello, world!")    
+                Text("Hello, world!")
+                Text("Hello, world!")    
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")  
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")  
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")  
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .padding()
+        VStack{
+            Text("Welcome")
+        }
+    }
+    
+  
+}
